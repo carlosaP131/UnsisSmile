4bd0ac2c49082b49a0ff3aa3e06f8877e02e1316
carlosaP131 <carlos.cantrax303@gmail.com>
2023-06-05 17:01:19 -0600

    Creacion del proyecto

---
+++ src/main/java/UnsisSmile/odonto/edu/UnsisSmile/UnsisSmileApplication.java
@@ -0,0 +1,13 @@
+package UnsisSmile.odonto.edu.UnsisSmile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsisSmileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnsisSmileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/UnsisSmile/odonto/edu/UnsisSmile/UnsisSmileApplicationTests.java
@@ -0,0 +1,13 @@
+package UnsisSmile.odonto.edu.UnsisSmile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnsisSmileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
