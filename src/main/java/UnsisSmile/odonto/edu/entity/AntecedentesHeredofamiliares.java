c33d5e5da7df0dae52108c1f515b16e11a20b57c
carlosaP131 <carlos.cantrax303@gmail.com>
2023-06-06 17:05:00 -0600

    Arreglado errores

--- src/main/java/UnsisSmile/odonto/edu/entity/AntecedentesHeredofamiliares.java
+++ src/main/java/UnsisSmile/odonto/edu/entity/AntecedentesHeredofamiliares.java
@@ -6,6 +6,7 @@ package UnsisSmile.odonto.edu.entity;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.Date;
 
 import jakarta.persistence.Basic;
@@ -15,7 +16,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 /**
@@ -84,8 +87,8 @@ public class AntecedentesHeredofamiliares implements Serializable {
     @Lob
     @Column(name = "problemas_cardiacos_amplio")
     private String problemasCardiacosAmplio;
-    @OneToMany(mappedBy = "fkIdAntecedentesHeredofamiliares")
-    private Collection<HistoriaClinicaGeneral> historiaClinicaGeneralCollection;
+    //@OneToMany(mappedBy = "fkIdAntecedentesHeredofamiliares")
+    //private Collection<HistoriaClinicaGeneral> historiaClinicaGeneralCollection;
 
     public AntecedentesHeredofamiliares() {
     }
@@ -230,14 +233,14 @@ public class AntecedentesHeredofamiliares implements Serializable {
         this.problemasCardiacosAmplio = problemasCardiacosAmplio;
     }
 
-    @XmlTransient
-    public Collection<HistoriaClinicaGeneral> getHistoriaClinicaGeneralCollection() {
-        return historiaClinicaGeneralCollection;
-    }
-
-    public void setHistoriaClinicaGeneralCollection(Collection<HistoriaClinicaGeneral> historiaClinicaGeneralCollection) {
-        this.historiaClinicaGeneralCollection = historiaClinicaGeneralCollection;
-    }
+//    @XmlTransient
+//    public Collection<HistoriaClinicaGeneral> getHistoriaClinicaGeneralCollection() {
+//        return historiaClinicaGeneralCollection;
+//    }
+//
+//    public void setHistoriaClinicaGeneralCollection(Collection<HistoriaClinicaGeneral> historiaClinicaGeneralCollection) {
+//        this.historiaClinicaGeneralCollection = historiaClinicaGeneralCollection;
+//    }
 
     @Override
     public int hashCode() {
