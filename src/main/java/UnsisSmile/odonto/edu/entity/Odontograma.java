17f1df48d9cda618223af3a9771caaf50a46bbb2
FernandoVite14 <jesush.vite@gmail.com>
2023-06-07 16:25:54 -0600

    Entitys modificados

--- src/main/java/UnsisSmile/odonto/edu/entity/Odontograma.java
+++ src/main/java/UnsisSmile/odonto/edu/entity/Odontograma.java
@@ -2,26 +2,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.unsis.odonto.edu.entity;
+package UnsisSmile.odonto.edu.entity;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Date;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 /**
  *
@@ -29,11 +27,11 @@ import javax.xml.bind.annotation.XmlTransient;
  */
 @Entity
 @Table(name = "odontograma")
-@XmlRootElement
-@NamedQueries({
-    @NamedQuery(name = "Odontograma.findAll", query = "SELECT o FROM Odontograma o"),
-    @NamedQuery(name = "Odontograma.findByIdOdontograma", query = "SELECT o FROM Odontograma o WHERE o.idOdontograma = :idOdontograma"),
-    @NamedQuery(name = "Odontograma.findByFecha", query = "SELECT o FROM Odontograma o WHERE o.fecha = :fecha")})
+//@XmlRootElement
+//@NamedQueries({
+//    @NamedQuery(name = "Odontograma.findAll", query = "SELECT o FROM Odontograma o"),
+//    @NamedQuery(name = "Odontograma.findByIdOdontograma", query = "SELECT o FROM Odontograma o WHERE o.idOdontograma = :idOdontograma"),
+//    @NamedQuery(name = "Odontograma.findByFecha", query = "SELECT o FROM Odontograma o WHERE o.fecha = :fecha")})
 public class Odontograma implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -46,14 +44,14 @@ public class Odontograma implements Serializable {
     @Column(name = "descripcion")
     private String descripcion;
     @Column(name = "fecha")
-    @Temporal(TemporalType.DATE)
+  //  @Temporal(TemporalType.DATE)
     private Date fecha;
-    @OneToMany(mappedBy = "fkIdOdontogramaInicial")
-    private Collection<HistoriaClinicaGeneral> historiaClinicaGeneralCollection;
-    @OneToMany(mappedBy = "fkIdOdontogramaFinal")
-    private Collection<HistoriaClinicaGeneral> historiaClinicaGeneralCollection1;
-    @OneToMany(mappedBy = "fkIdOdontograma")
-    private Collection<DienteDetalle> dienteDetalleCollection;
+//    @OneToMany(mappedBy = "fkIdOdontogramaInicial")
+//    private Collection<HistoriaClinicaGeneral> historiaClinicaGeneralCollection;
+//    @OneToMany(mappedBy = "fkIdOdontogramaFinal")
+//    private Collection<HistoriaClinicaGeneral> historiaClinicaGeneralCollection1;
+//    @OneToMany(mappedBy = "fkIdOdontograma")
+//    private Collection<DienteDetalle> dienteDetalleCollection;
 
     public Odontograma() {
     }
@@ -86,32 +84,32 @@ public class Odontograma implements Serializable {
         this.fecha = fecha;
     }
 
-    @XmlTransient
-    public Collection<HistoriaClinicaGeneral> getHistoriaClinicaGeneralCollection() {
-        return historiaClinicaGeneralCollection;
-    }
+//    @XmlTransient
+//    public Collection<HistoriaClinicaGeneral> getHistoriaClinicaGeneralCollection() {
+//        return historiaClinicaGeneralCollection;
+//    }
 
-    public void setHistoriaClinicaGeneralCollection(Collection<HistoriaClinicaGeneral> historiaClinicaGeneralCollection) {
-        this.historiaClinicaGeneralCollection = historiaClinicaGeneralCollection;
-    }
+//    public void setHistoriaClinicaGeneralCollection(Collection<HistoriaClinicaGeneral> historiaClinicaGeneralCollection) {
+//        this.historiaClinicaGeneralCollection = historiaClinicaGeneralCollection;
+//    }
 
-    @XmlTransient
-    public Collection<HistoriaClinicaGeneral> getHistoriaClinicaGeneralCollection1() {
-        return historiaClinicaGeneralCollection1;
-    }
+//    @XmlTransient
+//    public Collection<HistoriaClinicaGeneral> getHistoriaClinicaGeneralCollection1() {
+//        return historiaClinicaGeneralCollection1;
+//    }
 
-    public void setHistoriaClinicaGeneralCollection1(Collection<HistoriaClinicaGeneral> historiaClinicaGeneralCollection1) {
-        this.historiaClinicaGeneralCollection1 = historiaClinicaGeneralCollection1;
-    }
+//    public void setHistoriaClinicaGeneralCollection1(Collection<HistoriaClinicaGeneral> historiaClinicaGeneralCollection1) {
+//        this.historiaClinicaGeneralCollection1 = historiaClinicaGeneralCollection1;
+//    }
 
-    @XmlTransient
-    public Collection<DienteDetalle> getDienteDetalleCollection() {
-        return dienteDetalleCollection;
-    }
+//    @XmlTransient
+//    public Collection<DienteDetalle> getDienteDetalleCollection() {
+//        return dienteDetalleCollection;
+//    }
 
-    public void setDienteDetalleCollection(Collection<DienteDetalle> dienteDetalleCollection) {
-        this.dienteDetalleCollection = dienteDetalleCollection;
-    }
+//    public void setDienteDetalleCollection(Collection<DienteDetalle> dienteDetalleCollection) {
+//        this.dienteDetalleCollection = dienteDetalleCollection;
+//    }
 
     @Override
     public int hashCode() {
