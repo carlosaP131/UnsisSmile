6077169659541758f26fc5f46a34ae43ad0155cf
Niky11 <nancyobed11@gmail.com>
2023-06-08 10:05:49 -0600

    borrando clases prueba

--- src/main/java/UnsisSmile/odonto/edu/model/IHistoriaClinicaGeneralModelImpl.java
+++
@@ -1,9 +0,0 @@
-package UnsisSmile.odonto.edu.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import UnsisSmile.odonto.edu.entity.HistoriaClinicaGeneral;
-
-public interface IHistoriaClinicaGeneralModelImpl
-		extends JpaRepository<HistoriaClinicaGeneral, HistoriaClinicaGeneral> {
-
-}
--- src/main/java/UnsisSmile/odonto/edu/model/prueba.java
+++
@@ -1,5 +0,0 @@
-package UnsisSmile.odonto.edu.model;
-
-public class prueba {
-
-}
