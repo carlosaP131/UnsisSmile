b33ac4a8dd57b9d49b4a15ca95f3c6138fe44275
FernandoVite14 <jesush.vite@gmail.com>
2023-06-09 16:40:27 -0600

    controller modificado

--- src/main/java/UnsisSmile/odonto/edu/UnsisSmile/controller/HistoriaClinicaGeneralController.java
+++ src/main/java/UnsisSmile/odonto/edu/UnsisSmile/controller/HistoriaClinicaGeneralController.java
@@ -10,7 +10,7 @@ package UnsisSmile.odonto.edu.UnsisSmile.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,8 +24,8 @@ import UnsisSmile.odonto.edu.UnsisSmile.entity.HistoriaClinicaGeneral;
 import UnsisSmile.odonto.edu.UnsisSmile.service.HistoriaClinicaGeneralService;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
-@RequestMapping("/api")
+//@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/unsis")
 public class HistoriaClinicaGeneralController {
 	@Autowired // Spring se encarga de gestionar la creacion de los objetos
 	private HistoriaClinicaGeneralService service;
