7746627af2e563ad0e2d198641722bcc99f1a563
carlosaP131 <carlos.cantrax303@gmail.com>
2023-06-13 16:56:49 -0600

    Codigo basura eliminado y codigo formateado

--- src/main/java/UnsisSmile/odonto/edu/UnsisSmile/controller/SemestreGrupoController.java
+++ src/main/java/UnsisSmile/odonto/edu/UnsisSmile/controller/SemestreGrupoController.java
@@ -1,22 +1,20 @@
-/** ****************************************************************************
+/**
  *Autor:Carlos Aurelio Alcántara Pérez
  *Fecha de creación: 7-06-2023 ***
  *Fecha de actualización:13-06-2023
  *Descripción: Controller del Semestre grupo
  **
- * ****************************************************************************/
+ */
 package UnsisSmile.odonto.edu.UnsisSmile.controller;
 
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import UnsisSmile.odonto.edu.UnsisSmile.service.SemestreGrupoServiceImpl;
 
 @RestController
@@ -28,31 +26,37 @@ public class SemestreGrupoController {
 	 */
 	@Autowired
 	private SemestreGrupoServiceImpl service;
+
 	/**
 	 * Muestra la lista de grupos
+	 * 
 	 * @param semestreAux
 	 * @return
 	 */
 	@GetMapping("/filtrarGrupos/{semestreAux}")
 	public List<String> filtrarGrupos(@PathVariable String semestreAux) {
-        return service.filtrarGrupos(semestreAux);
-    }
+		return service.filtrarGrupos(semestreAux);
+	}
+
 	/**
 	 * Muestra un grupo
+	 * 
 	 * @return
 	 */
 	@GetMapping("/ListarSemestreGrupo")
-    public ArrayList<String>spListarSemestreGrupo() {
-        return service.spListarSemestreGrupo();
-    }
+	public ArrayList<String> spListarSemestreGrupo() {
+		return service.spListarSemestreGrupo();
+	}
+
 	/**
 	 * Muestra los grupos por semestre
+	 * 
 	 * @param semestreAux
 	 * @param grupoAux
 	 * @return
 	 */
 	@GetMapping("/obtenerIdSemestre/semestre/{semestreAux}/grupo/{grupoAux}")
-    public @ResponseBody int  filtrarIdSemestreGrupo(@PathVariable String semestreAux,@PathVariable String grupoAux) {
-        return service.filtrarIdSemestreGrupo(semestreAux, grupoAux);
-    }
+	public @ResponseBody int filtrarIdSemestreGrupo(@PathVariable String semestreAux, @PathVariable String grupoAux) {
+		return service.filtrarIdSemestreGrupo(semestreAux, grupoAux);
+	}
 }
--- src/main/java/UnsisSmile/odonto/edu/UnsisSmile/model/ISemestreGrupoImpl.java
+++ src/main/java/UnsisSmile/odonto/edu/UnsisSmile/model/ISemestreGrupoImpl.java
@@ -8,30 +8,25 @@
 package UnsisSmile.odonto.edu.UnsisSmile.model;
 
 import java.util.ArrayList;
-//import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.stereotype.Repository;
-
 import UnsisSmile.odonto.edu.UnsisSmile.entity.SemestreGrupo;
 
-
-
-
-
 @Repository
-public interface ISemestreGrupoImpl extends JpaRepository<SemestreGrupo,Long> {
-	
+public interface ISemestreGrupoImpl extends JpaRepository<SemestreGrupo, Long> {
+
 	/**
 	 * Procedimiento que Recibe un String de Semestre y regresa una lista de grupos
+	 * 
 	 * @param semestreAux
 	 * @return
 	 */
-	@Procedure (name = "filtrarGrupos")
+	@Procedure(name = "filtrarGrupos")
 	List<String> filtrarGrupos(String semestreAux);
+
 	/**
 	 * 
 	 * @param semestreAux
@@ -39,10 +34,11 @@ public interface ISemestreGrupoImpl extends JpaRepository<SemestreGrupo,Long> {
 	 * @return
 	 */
 	@Query(value = "select unsis_smile.filtrarIdSemestreGrupo(:semestreAux, :grupoAux);\n", nativeQuery = true)
-	int filtrarIdSemestreGrupo(String semestreAux,String grupoAux);
-	
+	int filtrarIdSemestreGrupo(String semestreAux, String grupoAux);
+
 	/**
-	 *Procedimiento que Recibe un Int de Id y regresa  una lista de grupos
+	 * Procedimiento que Recibe un Int de Id y regresa una lista de grupos
+	 * 
 	 * @return
 	 */
 	@Procedure(name = "spListarSemestreGrupo")
