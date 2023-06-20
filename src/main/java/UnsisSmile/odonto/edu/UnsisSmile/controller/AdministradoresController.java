2e3fb84458987e7299c021b25a8f7b1bfe6f0049
Tu Nombre <you@example.com>
2023-06-20 16:58:15 -0600

    cambios

--- src/main/java/UnsisSmile/odonto/edu/UnsisSmile/controller/AdministradoresController.java
+++ src/main/java/UnsisSmile/odonto/edu/UnsisSmile/controller/AdministradoresController.java
@@ -11,6 +11,7 @@ package UnsisSmile.odonto.edu.UnsisSmile.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,7 @@ import UnsisSmile.odonto.edu.UnsisSmile.service.AdministadoresService;
  * @author labingsw03
  *
  */
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/unsis")
 public class AdministradoresController {
 
